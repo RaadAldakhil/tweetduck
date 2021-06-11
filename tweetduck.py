0d779b3c69dd7d0c4f8f1be4225afbef01c3e6da
Ra'ad Aldakhil <aldakhil95@gmail.com>
2021-06-11 14:51:43 +0300

    add duck tweeting in main

---
+++ tweetduck.py
@@ -0,0 +1,17 @@
+import tweepy
+import logging
+from config import create_api
+import time
+
+logging.basicConfig(Level=logging.INFO)
+logger = logging.getLogger()
+
+def main():
+    api = create_api()
+
+    # Create a tweet
+    api.update_status(os.system('fortune -n 130 | cowsay -f duck'))
+
+if __name__ == "__main__":
+    main()
+
