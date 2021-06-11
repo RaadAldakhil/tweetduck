9b00d3be4a5d399bbef94610d16a5b8d5a8c1f8d
Ra'ad Aldakhil <aldakhil95@gmail.com>
2021-06-11 14:46:54 +0300

    api config

--- tweepy.py
+++ tweepy.py
@@ -1,12 +1,23 @@
 import tweepy
+import logging
 import os
 
-# Authenticate to Twitter
-auth = tweepy.OAuthHandler("CONSUMER_KEY", "CONSUMER_SECRET")
-auth.set_access_token("ACCESS_TOKEN", "ACCESS_TOKEN_SECRET")
+logger = logging.getLogger()
 
-# Create API object
-api = tweepy.API(auth)
+def create_api():
+    consumer_key = os.getenv("CONSUMER_KEY")
+    consumer_secret = os.getenv("CONSUMER_SECRET")
+    access_token = os.getenv("ACCESS_TOKEN")
+    access_token_secret = os.getenv("ACCESS_TOKEN_SECRET")
 
-# Create a tweet
-api.update_status(os.system('fortune -n 130 | cowsay -f duck'))
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+    api = tweepy.API(auth, wait_on_rate_limit=True, 
+        wait_on_rate_limit_notify=True)
+    try:
+        api.verify_credentials()
+    except Exception as e:
+        logger.error("Error creating API", exc_info=True)
+        raise e
+    logger.info("API created")
+    return api
