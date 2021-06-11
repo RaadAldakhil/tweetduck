3a7113522d125824612c041eaf1c9b781f48dfbe
Ra'ad Aldakhil <aldakhil95@gmail.com>
2021-06-11 15:20:28 +0300

    pipe duck fortune into tweet instead of print duck fortune on cli

--- tweetduck.py
+++ tweetduck.py
@@ -9,9 +9,12 @@ logger = logging.getLogger()
 
 def main():
     api = create_api()
-
+    
+    # Generate duck fortune
+    stream = os.popen('fortune -n 130 | cowsay -f duck')
+    output = stream.readLines()
     # Create a tweet
-    api.update_status(os.system('fortune -n 130 | cowsay -f duck'))
+    api.update_status(output)
 
 if __name__ == "__main__":
     main()
