23c59814b466149dcd35d23cf5935101171d9e78
Ra'ad Aldakhil <aldakhil95@gmail.com>
2021-06-11 15:29:07 +0300

    fix string

--- tweetduck.py
+++ tweetduck.py
@@ -14,7 +14,7 @@ def main():
     stream = os.popen('fortune -n 120 | cowsay -f duck')
     output = stream.read()
     # Create a tweet
-    api.update_status(output.append(" \n-Made with Tweepy"))
+    api.update_status(output + "\n-Made with Tweepy")
 
 if __name__ == "__main__":
     main()
